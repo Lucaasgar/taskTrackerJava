523aad7843201cb81f441155b748c68adaae9888
Lucaasgar <aglucasp@gmail.com>
2025-02-03 00:32:18 -0300

    end class task

--- src/main/java/org/example/Task.java
+++ src/main/java/org/example/Task.java
@@ -3,19 +3,33 @@ package org.example;
 import java.time.LocalDateTime;
 
 public class Task {
+    private static int lastId = 0;
     private int id;
     private String description;
     private Status status;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
-    public Task(int id, String description, Status status, LocalDateTime createdAt, LocalDateTime updatedAt) {
-        this.id = id;
+    public Task(String description) {
+        this.id = ++lastId;
         this.description = description;
-        this.status = status;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
+        this.status = Status.TODO;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
     }
 
+    public void markInProgress(){
+        this.status = Status.IN_PROGRESS;
+        this.updatedAt = LocalDateTime.now();
+    }
 
+    public void markDone(){
+        this.status = Status.DONE;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void updateDescription(String desc){
+        this.description = desc;
+        this.updatedAt = LocalDateTime.now();
+    }
 }
